469f95288a5d6e15f31b2905e3d3dceadc479675
7up <23521320@gm.uit.edu.vn>
2025-05-22 14:57:23 +0700

    upload template spring boot

---
+++ backendQLKS/src/main/java/com/example/backendqlks/BackendQlksApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendqlks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendQlksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendQlksApplication.class, args);
+    }
+
+}
